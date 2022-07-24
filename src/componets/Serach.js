3ec92906dc199af7f2290083e639d9355f3a148d
anajlaoui007 <anajlaoui007@gmail.com>
2022-07-24 12:33:27 +0100

    Add folder react hooks

---
+++ src/componets/Serach.js
@@ -0,0 +1,13 @@
+import StarRatingComponent from 'react-star-rating-component';
+import React from 'react'
+
+const Search = ({setSearch, setRate}) => {
+  return (
+    <div>
+        <input placeholder='Search' onChange={(e)=>setSearch(e.target.value)}></input>
+        <StarRatingComponent name='rate' onStarClick={(star)=>setRate(star)} />
+    </div>
+  )
+}
+
+export default Search
